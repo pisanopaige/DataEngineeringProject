317ceeb626e9924d2a32eee768c9ce09e5dbb37a
“Paige <pisanopaige@vt.edu>
2024-11-01 16:40:29 -0400

    Updated batch_ingest.py

--- batch_ingest.py
+++ batch_ingest.py
@@ -22,7 +22,7 @@ def ingest_data():
 
     s3 = S3FileSystem()
     # S3 bucket directory
-    DIR = 's3://ece5984-s3-pisanopaige/DataEngineeringProject/'
+    DIR = 's3://ece5984-s3-pisanopaige/DataEngineeringProject/batch_ingest'
 
     # Push train and test data to S3 bucket as pickle files
     with s3.open('{}/{}'.format(DIR, 'train_data.pkl'), 'wb') as f_train:
