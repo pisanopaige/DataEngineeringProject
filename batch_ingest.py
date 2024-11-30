61a44b04c51315b19a2e2912a4cba0d00512132c
“Paige <pisanopaige@vt.edu>
2024-11-30 14:04:34 -0500

    Updated code

--- batch_ingest.py
+++ batch_ingest.py
@@ -1,33 +1,41 @@
-import kagglehub
+import os
+import pickle
 import pandas as pd
 from s3fs import S3FileSystem
-import pickle
 import zipfile
-import os
+from kaggle.api.kaggle_api_extended import KaggleApi
 
 def ingest_data():
-    # Load fraud dataset from Kaggle
-    path = kagglehub.dataset_download("kartik2112/fraud-detection")
+    # Initialize Kaggle API client
+    api = KaggleApi()
+    api.authenticate()
 
-    # Unzip the downloaded zip file
-    zip_file_path = 'archive.zip'
-    with zipfile.ZipFile(zip_file_path, 'r') as zip_file:
-        zip_file.extractall()
+    # Define dataset and download path
+    dataset = "kartik2112/fraud-detection"
+    download_path = '/tmp/fraud-detection'
+
+    # Download dataset from Kaggle
+    api.dataset_download_files(dataset, path=download_path, unzip=True)
+
+    # Define the path for the extracted data
+    train_dataset_path = os.path.join(download_path, 'fraudTrain.csv')
+    test_dataset_path = os.path.join(download_path, 'fraudTest.csv')
 
     # Load datasets into pandas dataframes
-    train_dataset_path = os.path.join(path, 'archive', 'fraudTrain.csv')
-    test_dataset_path = os.path.join(path, 'archive', 'fraudTest.csv')
     train_data = pd.read_csv(train_dataset_path)
     test_data = pd.read_csv(test_dataset_path)
 
+    # Initialize S3 client
     s3 = S3FileSystem()
-    # S3 bucket directory
+
+    # Define the S3 directory
     DIR = 's3://ece5984-s3-pisanopaige/DataEngineeringProject/batch_ingest'
 
-    # Push train and test data to S3 bucket as pickle files
-    with s3.open('{}/{}'.format(DIR, 'train_data.pkl'), 'wb') as f_train:
+    # Push train and test data to S3 as pickle files
+    with s3.open(f'{DIR}/train_data.pkl', 'wb') as f_train:
         f_train.write(pickle.dumps(train_data))
-    with s3.open('{}/{}'.format(DIR, 'test_data.pkl'), 'wb') as f_test:
+
+    with s3.open(f'{DIR}/test_data.pkl', 'wb') as f_test:
         f_test.write(pickle.dumps(test_data))
 
 if __name__ == "__main__":
