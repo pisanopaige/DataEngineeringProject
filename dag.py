fce3f53e1c60b6846eb3cf36f7ad9e0a19e4636c
“Paige <pisanopaige@vt.edu>
2024-12-02 16:27:12 -0500

    Updated code

--- dag.py
+++ dag.py
@@ -6,8 +6,8 @@ from datetime import datetime
 from batch_ingest import ingest_data
 from transform import transform_data
 from train_model import train_and_save_model
-from feature_extract import feature_extract  # Added the feature extraction import
-# from visualize import visualize_data  # Commented out visualize task
+from feature_extract import feature_extract
+from sql_load import upload_data_to_sql
 
 default_args = {
     'owner': 'pisanopaige',
@@ -23,42 +23,41 @@ default_args = {
 dag = DAG(
     'fraud_detection_dag',
     default_args=default_args,
-    description='A pipeline for credit card fraud detection that includes batch ingestion, feature extraction, data transformation, model training, and visualization',
+    description='A pipeline for credit card fraud detection that includes batch ingestion, feature extraction, data transformation, model training, and SQL data upload',
     schedule_interval=timedelta(days=1),
-    catchup=False  # Ensures the DAG doesn't run for previous days
+    catchup=False
 )
 
 # Define tasks
-ingest_task = PythonOperator(
+ingest_etl = PythonOperator(
     task_id='ingest_dataset',
     python_callable=ingest_data,
     dag=dag,
 )
 
-transform_task = PythonOperator(
+transform_etl = PythonOperator(
     task_id='transform_dataset',
     python_callable=transform_data,
     dag=dag,
 )
 
-feature_extract_task = PythonOperator(
+feature_extract_etl = PythonOperator(
     task_id='feature_extract',
     python_callable=feature_extract,
     dag=dag,
 )
 
-train_task = PythonOperator(
+train_etl = PythonOperator(
     task_id='train_model',
     python_callable=train_and_save_model,
     dag=dag,
 )
 
-# Commented out the visualize task for now
-# visualize_task = PythonOperator(
-#     task_id='visualize_data',
-#     python_callable=visualize_data,  # Assuming you have this function defined
-#     dag=dag,
-# )
+sql_load_etl = PythonOperator(
+    task_id='sql_load',
+    python_callable=upload_data_to_sql,
+    dag=dag,
+)
 
 # Set task dependencies
-ingest_task >> transform_task >> feature_extract_task >> train_task
+ingest_etl >> transform_etl >> feature_extract_etl >> train_etl >> sql_load_etl
